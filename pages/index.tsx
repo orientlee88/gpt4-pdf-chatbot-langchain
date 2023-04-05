6badb9f84ec6b353f38c03affc5b77ab50f4be19
orientlee88 <36152639+orientlee88@users.noreply.github.com>
2023-04-05 09:29:42 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -165,7 +165,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            业务领先战略 ChatBot
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -206,7 +206,7 @@ export default function Home() {
                     <>
                       <div key={`chatMessage-${index}`} className={className}>
                         {icon}
-                        <div className={styles.markdownanswer}>
+                        <div className={`${styles.markdownanswer} break-words`}>
                           <ReactMarkdown linkTarget="_blank">
                             {message.message}
                           </ReactMarkdown>
@@ -319,8 +319,8 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          <a href="cc:lidf@brainwork.club">
+            Powered by ChatGPT. Demo built by 领先2035 李东方 (Email: lidf@brainwork.club 单击链接复制邮箱地址).
           </a>
         </footer>
       </Layout>
